dd6d53e3fdf10d6ae89d6345a409564fac1b6ef2
shivansh <shankarshivansh28@gmail.com>
2026-02-22 20:23:01 +0530

    DB AND ENV SETUP

--- backend/src/lib/ENV.js
+++ backend/src/lib/ENV.js
@@ -1,5 +1,5 @@
 import dotenv from "dotenv"; // Load environment variables from .env file
-dotenv.config(); // Load environment variables from .env fil
+dotenv.config({silent: true}); 
 export const ENV = {
     PORT: process.env.PORT,
     DB_URL: process.env.DB_URL,
